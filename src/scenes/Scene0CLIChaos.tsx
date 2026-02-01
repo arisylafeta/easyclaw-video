3df2e767262dc14504e29cde81415edd83e98508
Arianit Sylafeta <ari.sylafeta@gmail.com>
2026-02-01 21:06:12 +0000

    Update Scene0CLIChaos to use GridBackground with showBlobs

--- src/scenes/Scene0CLIChaos.tsx
+++ src/scenes/Scene0CLIChaos.tsx
@@ -223,34 +223,7 @@ export const Scene0CLIChaos: React.FC = () => {
         />
       </div>
 
-      <GridBackground color={COLORS.grid} size="100px" />
-
-      {/* Faint greenish-blue blob */}
-      <div
-        style={{
-          position: "absolute",
-          width: "800px",
-          height: "800px",
-          left: "-200px",
-          top: "50%",
-          transform: "translateY(-50%)",
-          background: "radial-gradient(circle, rgba(34, 197, 94, 0.2) 0%, transparent 70%)",
-          pointerEvents: "none",
-        }}
-      />
-
-      {/* Faint orangeish-red blob */}
-      <div
-        style={{
-          position: "absolute",
-          width: "900px",
-          height: "900px",
-          right: "-300px",
-          top: "30%",
-          background: "radial-gradient(circle, rgba(239, 68, 68, 0.2) 0%, transparent 70%)",
-          pointerEvents: "none",
-        }}
-      />
+      <GridBackground color={COLORS.grid} size="100px" showBlobs />
     </div>
   );
 };
